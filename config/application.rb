592cf726170524e6d82375996e30ed0344c3cd92
AndModestov <and.modestov@gmail.com>
2017-07-13 17:34:48 +0300

    finish login & add pad creating for publisher

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,10 @@ require_relative '../lib/my_target/publisher'
 pwd = Dir.pwd.split('/').take_while { |s| s != 'advertising_bot'}.push('advertising_bot').join('/')
 Dotenv.load("#{pwd}/.env")
 
-publisher = MyTarget::Publisher.new 'andrey@imcimc.ru', 'balalaika2000'
-publisher.login
+login = 'andrey@imcimc.ru'
+password = 'balalaika2000'
+pad_url = 'https://itunes.apple.com/us/app/angry-birds/id343200656?mt:8'
+
+publisher = MyTarget::Publisher.new login, password, pad_url
+publisher.authenticate
+publisher.create_pad
